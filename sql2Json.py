77ecd91d2e1468dc50639b1d94f84cc9f9b1e180
thy4205 <thy4205@gmail.com>
2021-12-31 15:37:26 +0800

    Reduce Memory Usage; The script now creats a bunch of JSON files instead of one single JSON file, in order to reduce the load time and memeory usage in other program. Each JSON file size can be adjusted with rowChunk; which is number of row per JSON.

--- sql2Json.py
+++ sql2Json.py
@@ -1,6 +1,7 @@
 import pyodbc
 import json
 import datetime
+import math
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
@@ -12,30 +13,48 @@ class DateEncoder(json.JSONEncoder):
             return obj.strftime("%Y-%m-%d")  
         else:  
             return json.JSONEncoder.default(self, obj) 
-server = '192.168.1.20' #your servers IP or DNS Hostname
-database = 'kf3'    #Your desired Database you want to create JSON from
-username = 'kf3migrate' #Username for migration, make sure the user has read access
-password = 'kf3migrate' #Password, self explanatory
-tablename = 'kfpolclaim' #Table Name,table to create JSON from
-querylimit = '' #If you want to add conditions to the qurery
+server = 'sqlserver' #your servers IP or DNS Hostname
+database = 'databaseName'    #Your desired Database you want to create JSON from
+username = 'UserName' #Username for migration, make sure the user has read access
+password = 'Password' #Password, self explanatory
+tablenames = ['tableName'] #Table Name,table to create JSON from
+rowChunks = 25000
 
 #Make sure you have installed and activate the required OBDC driver and change the next line
 cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 #Sample select query
-cursor.execute("SELECT  * from " + tablename +' '+querylimit) 
-rows = cursor.fetchall()
-a_dict = dict()
-a_dict["tablename"] = tablename
-a_dict["columns"] =[column[0] for column in cursor.description]
-cname = []
-for row in cursor.columns(table=tablename):
-     cname.append([x for x in row][5])
-a_dict["datatype"] = cname
-data = []
-for row in rows:
-    data.append([x for x in row]) # or simply data.append(list(row))
-a_dict['rows']=data
-#with open(tablename+'.json', 'w',encoding='utf8') as f:
-with open('import.json', 'w',encoding='utf8') as f:
-    json.dump(a_dict, f,cls=DateEncoder,ensure_ascii=False)
+
+for tablename in tablenames:
+    cursor.execute("SELECT * from " + tablename) 
+    rows = cursor.fetchall()
+    a_dict = dict()
+    a_dict["tablename"] = tablename
+    a_dict["columns"] =[column[0] for column in cursor.description]
+    a_dict["nullable"] =[column[6] for column in cursor.description]
+    rowCountSum = len(rows)
+    timeToRun = math.ceil(rowCountSum / rowChunks)
+    cname = []
+    colSize = []
+    i = 0;
+    
+
+    for row in cursor.columns(table=tablename):
+         cname.append([x for x in row][5])
+         colSize.append([x for x in row][7])
+       
+    a_dict["datatype"] = cname
+    a_dict["columnSize"] = colSize
+
+    for i in range(timeToRun):
+        j = i
+        j = i * rowChunks
+        k = j + rowChunks
+       
+        data = []
+        for row in rows[j:k]:
+            data.append([x for x in row]) # or simply data.append(list(row))
+        a_dict['rows']=data
+        #with open(tablename+'.json', 'w',encoding='utf8') as f:
+        with open('import_'+tablename+'_'+str(i).zfill(4)+'.json', 'w',encoding='utf8') as f:
+            json.dump(a_dict, f,cls=DateEncoder,ensure_ascii=False)
